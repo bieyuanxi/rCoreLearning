b416f8189197bb4d26b1d87c8974f85ce176866b
Terra <bieyuanxi@163.com>
2025-03-07 12:00:08 +0800

    c1

---
+++ os/src/lang_items.rs
@@ -0,0 +1,17 @@
+use core::panic::PanicInfo;
+use crate::{println, sbi::shutdown};
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message()
+        );
+    } else {
+        println!("Panicked: {}", info.message());
+    }
+    shutdown(true)
+}
