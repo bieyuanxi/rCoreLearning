5236f6a3faa0d92579a14b3d799961d16a218596
Terra <bieyuanxi@163.com>
2025-03-08 11:21:49 +0800

    add base support for log

--- os/src/main.rs
+++ os/src/main.rs
@@ -11,9 +11,27 @@ global_asm!(include_str!("entry.asm"));
 
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
+    unsafe extern "C" {
+        fn stext();
+        fn etext();
+        fn sdata();
+        fn edata();
+        fn srodata();
+        fn erodata();
+        fn sbss();
+        fn ebss();
+    }
     clear_bss();
     println!("Hello world!");
+    error!("this is an error!");
+    info!("this is an info! info: {}", "msg");
+    debug!("this is a debug!");
+    info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    info!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    info!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     panic!("Shutdown machine!");
+    
 }
 
 fn clear_bss() {
